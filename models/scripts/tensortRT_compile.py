6a829ba62fb02eaca4eec6ccc2dc63e32c65947a
flo_tour <fg44470@gmail.com>
2026-05-16 15:40:13 +0200

    Fix model paths in TensorRT compile script for correct export functionality

--- models/scripts/tensortRT_compile.py
+++ models/scripts/tensortRT_compile.py
@@ -1,6 +1,12 @@
 from ultralytics import YOLO
 
-YOLO('yolo26m-pose.pt', task='pose').export(
+YOLO('models/yolo26m-pose.pt', task='pose').export(
+    format='engine',
+    device=0,
+    half=True,
+    simplify=True)
+
+YOLO('models/yolo26l.pt', task='detect').export(
     format='engine',
     device=0,
     half=True,
